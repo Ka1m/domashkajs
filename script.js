8773f657a93262908cc2cf70a30a012c95a425d2
kaim <kaim123@.ru>
2021-02-28 12:13:37 +0500

    save

---
+++ script.js
@@ -0,0 +1,21 @@
+var name = prompt('введите имя');
+
+
+var age = +prompt('введите свой  возраст');
+
+
+var plus = +prompt('1 + 1 = ');
+var minus = +prompt('2 - 1 =');
+var xxxx = +prompt('2 * 2 =');
+var delenie = +prompt('8 / 4 =');
+var ostatook = +prompt('14 % 3 =');
+
+console.log('ваше имя ' + name);
+console.log('ваш возраст ' + age);
+
+console.log('1 + 1 = ' + (1 + 1) + ' ; ваш ответ ' + plus);
+console.log('2 - 1 = ' + (2 - 1) + ' ; ваш ответ ' + minus);
+console.log('2 * 2 = ' + (2 * 2) + ' ; ваш ответ ' + xxxx);
+console.log('8 / 4 = ' + (8 / 4) + ' ; ваш ответ ' + delenie);
+console.log('14 % 3 = ' + (14 % 3) + ' ; ваш ответ ' + ostatook);
+/* alert('ваше имя' + str1); */
